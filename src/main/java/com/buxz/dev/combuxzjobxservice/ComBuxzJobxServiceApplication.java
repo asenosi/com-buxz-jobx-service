5f12ff86f0aee1020aadc40a58a67c86526ee21c
Aobakwe Senosi <senosiag@gmail.com>
2025-10-15 23:27:28 +0200

    Fix configuration properties scanning for tests

--- src/main/java/com/buxz/dev/combuxzjobxservice/ComBuxzJobxServiceApplication.java
+++ src/main/java/com/buxz/dev/combuxzjobxservice/ComBuxzJobxServiceApplication.java
@@ -2,11 +2,13 @@ package com.buxz.dev.combuxzjobxservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class   ComBuxzJobxServiceApplication {
+@ConfigurationPropertiesScan
+public class ComBuxzJobxServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComBuxzJobxServiceApplication.class, args);
